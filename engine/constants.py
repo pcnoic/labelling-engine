1a6825dabada02d25a40bd9fd261ade0a296c96c
Christos Alexiou <pcnoic@gmail.com>
2021-05-30 16:42:28 +0300

    Introduce contstants

---
+++ engine/constants.py
@@ -0,0 +1,5 @@
+DATABASES_PATH="../tmp/storage"
+CREDENTIALS_PATH_BUCKETS="../credentials/tynr/engine/bucketAccess.json"
+CREDENTIALS_PATH_FIRESTORE = '../credentials/tynr/engine/firestoreServiceAccount.json'
+INGESTION_COLLECTION = "tyns"
+INGESTION_BATCH_LIMIT = 1000
