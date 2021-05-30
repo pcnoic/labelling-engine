4627663d53f62072305340e4c7cef74fda9e58c3
Christos Alexiou <pcnoic@gmail.com>
2021-05-30 16:43:36 +0300

    refactor: renaming and constant extraction

--- engine/adapter/firestore_auth.py
+++ engine/adapter/firestore_auth.py
@@ -2,19 +2,18 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from constants import CREDENTIALS_PATH_FIRESTORE
 
 # Using a service account for authenticating against firestore 
 
-CREDENTIALS_PATH = '../credentials/tynr/engine/firestoreServiceAccount.json'
-
-def get_credentials_path():
+def get_fs_credentials_path():
     dirname = os.path.dirname(__file__)
-    credentials_path = os.path.join(dirname, CREDENTIALS_PATH)   
+    credentials_path = os.path.join(dirname, CREDENTIALS_PATH_FIRESTORE)   
     
     return credentials_path 
     
-def fs_getAuth():
-    cred = credentials.Certificate(get_credentials_path())
+def fs_getClient():
+    cred = credentials.Certificate(get_fs_credentials_path())
     firebase_admin.initialize_app(cred)
 
     return firestore.client()
